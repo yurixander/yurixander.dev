ada8465ad0cc2acd8c262fd954d4e9c43905fa93
yurixander <101931215+yurixander@users.noreply.github.com>
2022-09-14 22:26:07 -0400

    Use vite, update resume PDF

---
+++ src/site.ts
@@ -0,0 +1,260 @@
+const consts = {
+  cursorSize: 20,
+  emailRegex: /^[^@]+@[^@]+\.[^@]+$/,
+  confirmationRegex: /^(y|n)$/,
+  terminalCommands: [
+    ["help", "Shows this help list."],
+    ["clear", "Clear the console."],
+    ["send-msg", "Re-run the message prompt."],
+    ["view-resume", "Opens the Resume."],
+    ["view-source", "Open the GitHub repository for this website."]
+  ],
+  parallaxMaxDistance: 30,
+  // This helps prevent crawler bots, and thus possible spam.
+  email: "eXVyaXhhbmRlci5yaWNhcmRvQG91dGxvb2suY29t",
+  starSpawnInterval: 500,
+  terminalKeystrokeInterval: 100
+}
+
+type Vector2 = {
+  x: number,
+  y: number
+}
+
+type Dimensions = {
+  width: number,
+  height: number
+}
+
+function calculateCenterPoint(position: Vector2, size: number): Vector2 {
+  return {
+    x: position.x - size / 2,
+    y: position.y - size / 2
+  }
+}
+
+function isInViewport($element: HTMLElement): boolean {
+  const rect = $element.getBoundingClientRect()
+
+  return (
+    rect.x >= 0 &&
+    rect.y >= 0 &&
+    rect.x <= (window.innerWidth || document.documentElement.clientWidth) &&
+    rect.y <= (window.innerHeight || document.documentElement.clientHeight)
+  )
+}
+
+type State = {
+  $cursor: HTMLElement,
+  $clickAudio: HTMLAudioElement,
+  $lastActiveGuideDot: HTMLElement,
+  $snapTarget: HTMLElement,
+  $contactSection: HTMLElement,
+  $darkSensitives: NodeListOf<HTMLElement>,
+  lastScrollHeight: number,
+  contactTerminal: Terminal,
+  isHoldingClick: boolean
+}
+
+window.addEventListener("load", () => {
+  let state: State = {
+    $cursor: document.getElementById("cursor"),
+    $clickAudio: document.querySelector("[data-asset='click']") as HTMLAudioElement,
+    // NOTE: This will select the first element matching this selector.
+    $lastActiveGuideDot: document.querySelector(".guide > .dot"),
+    isHoldingClick: false,
+    $snapTarget: null,
+    $contactSection: document.getElementById("contact"),
+    $darkSensitives: document.querySelectorAll("[data-dark-sensitive]"),
+    contactTerminal: new Terminal(document.querySelector<HTMLElement>(".terminal > .content")),
+    lastScrollHeight: 0,
+  }
+
+  // Initialization.
+  console.clear()
+
+  console.log(`👋 Hi! Interested in looking at the source code? Go ahead! If you have any questions or concerns about this site, feel free to reach me at ${atob(consts.email)}.`)
+
+  contactPrompt(state.contactTerminal)
+  state.contactTerminal.isReadonly = false
+
+  // Spawn stars.
+  const starSpawnLocations = ["contact"]
+
+  setInterval(() => {
+    starSpawnLocations.forEach((spawnLocationId) => {
+      const $targetLocation = document.getElementById(spawnLocationId)
+      const $star = document.querySelector(".obj-star").cloneNode(true) as HTMLElement
+      const duration = (Math.random() * 10) + 2
+
+      $star.addEventListener("mouseenter", (_e) => {
+        // TODO: Apply velocity in random direction?
+      })
+
+      $star.style.animationDuration = `${duration}s`
+      $star.style.left = `${Math.random() * window.innerWidth}px`
+      $star.style.top = `${Math.random() * $targetLocation.offsetHeight}px`
+      $targetLocation.prepend($star)
+      setTimeout(() => $star.remove(), duration * 1000)
+    })
+  }, consts.starSpawnInterval)
+
+  document.querySelectorAll("button[data-href]").forEach(($btn) => {
+    $btn.addEventListener("click", () => window.open($btn.getAttribute("data-href"), "_blank"))
+  })
+
+  document.querySelectorAll("a, button").forEach(($clickable) => {
+    $clickable.addEventListener("click", () => {
+      state.$clickAudio.play()
+    })
+  })
+
+  // FIXME: This follows cursor so it doesn't work perfectly well.
+  document.querySelectorAll("[data-is-dark]").forEach(($dark) => {
+    $dark.addEventListener("mouseenter", () => state.$darkSensitives.forEach(($darkSensitive) => $darkSensitive.classList.add("meta-dark")))
+    $dark.addEventListener("mouseleave", () => state.$darkSensitives.forEach(($darkSensitive) => $darkSensitive.classList.remove("meta-dark")))
+  })
+
+  document.querySelectorAll<HTMLElement>(".guide > .dot").forEach(($guideDot) => {
+    $guideDot.addEventListener("click", () => {
+      if (state.$lastActiveGuideDot === $guideDot)
+        return
+
+      state.$lastActiveGuideDot.classList.remove("active")
+      $guideDot.classList.add("active")
+      state.$lastActiveGuideDot = $guideDot
+    })
+  })
+
+  // CONSIDER: Default to all links, then use opt-out?
+  document.querySelectorAll<HTMLElement>("[data-snappable], nav a, p a, button").forEach(($snappable) => {
+    $snappable.addEventListener("mouseenter", () => {
+      // BUG: When you scroll a section, the pointer snap is misplaced.
+      const snappablePosition = $snappable.getBoundingClientRect()
+
+      state.$snapTarget = $snappable
+      state.$cursor.style.left = `${snappablePosition.x}px`
+      state.$cursor.style.top = `${snappablePosition.y}px`
+      state.$cursor.style.width = `${$snappable.clientWidth || $snappable.offsetWidth}px`
+      state.$cursor.style.height = `${$snappable.clientHeight || $snappable.offsetHeight}px`
+      state.$cursor.classList.add("snapped")
+    })
+
+    $snappable.addEventListener("mouseleave", () => {
+      if (state.$snapTarget === $snappable) {
+        state.$snapTarget = null
+        state.$cursor.style.width = `${consts.cursorSize}px`
+        state.$cursor.style.height = `${consts.cursorSize}px`
+        state.$cursor.classList.remove("snapped")
+      }
+    })
+  })
+
+  // TODO: Finish implementing.
+  // document.querySelectorAll("[data-parallax]").forEach(($parallax) => {
+  //   window.addEventListener("scroll", () => {
+  //     // if (isInViewport($parallax)) {
+  //     //   alert("In viewport")
+  //     // }
+  //     // else console.log("Out of view")
+  //     const currentTopValue = parseInt($parallax.style.top) || 0
+  //     const step = 1
+
+  //     const direction = document.body.scrollTop - state.lastScrollHeight > 0 ? 1 : -1
+
+  //     state.lastScrollHeight = document.body.scrollTop
+  //     $parallax.style.top = (currentTopValue + step) * direction + "px"
+
+  //     // console.log(window.pageYOffset)
+  //   })
+  // })
+
+  // Obfuscate email & personal information from crawlers.
+  document.querySelectorAll("[data-href-email]").forEach(($email) => {
+    $email.addEventListener("click", () => window.open(`mailto:${atob(consts.email)}`, "_self"))
+  })
+
+  // Parallax effect.
+  document.querySelectorAll<HTMLElement>("[data-parallax]").forEach(($parallax) => {
+    window.addEventListener("scroll", () => {
+      const scrollDistance = window.scrollY
+
+      $parallax.style.transform = `translateY(${scrollDistance}px)`
+      console.log($parallax)
+    })
+  })
+
+  // Scroll effect.
+  const scrollObserver = new IntersectionObserver((entries) => entries.forEach((entry) => {
+    const scrollAnimation = entry.target.getAttribute("data-scroll-animation")
+    const hasScrollAnimation = scrollAnimation !== null
+
+    if (entry.isIntersecting) {
+      entry.target.classList.remove("meta-hidden")
+      entry.target.classList.add("meta-visible")
+
+      if (hasScrollAnimation)
+        entry.target.setAttribute("data-animation", scrollAnimation)
+    }
+    else {
+      entry.target.classList.remove("meta-visible")
+      entry.target.classList.add("meta-hidden")
+
+      if (hasScrollAnimation)
+        entry.target.removeAttribute("data-animation")
+    }
+  }), {
+    // rootMargin: "500px",
+    // threshold: 1.0
+  })
+
+  document.querySelectorAll("p").forEach(($p) => {
+    $p.setAttribute("data-scroll-animation", "text-appear")
+  })
+
+  document.querySelectorAll("[data-has-scroll-effect], [data-scroll-animation]").forEach(($scrollEffectEl) => scrollObserver.observe($scrollEffectEl))
+
+  // Event listeners.
+  window.addEventListener("scroll", () => {
+    if (isInViewport(state.contactTerminal.$terminal))
+      state.contactTerminal.$terminal.focus()
+    // TODO: Only need to call the `blur` action when it was focused.
+    else
+      state.contactTerminal.$terminal.blur()
+  })
+
+  window.addEventListener("mousemove", (e) => {
+    if (state.$snapTarget !== null)
+      return
+
+    const nextPosition = calculateCenterPoint({
+      x: e.x,
+      y: e.y
+    }, consts.cursorSize)
+
+    state.$cursor.style.left = `${nextPosition.x}px`
+    state.$cursor.style.top = `${nextPosition.y}px`
+  })
+
+  window.addEventListener("mousedown", () => {
+    state.isHoldingClick = true
+    state.$cursor.classList.add("click")
+  })
+
+  window.addEventListener("mouseup", () => {
+    state.isHoldingClick = false
+    state.$cursor.classList.remove("click")
+  })
+
+  window.addEventListener("mousein", () => {
+    state.$cursor.classList.remove("hidden")
+  })
+
+  window.addEventListener("blur", () => {
+    document.body.classList.add("blur")
+  })
+
+  window.addEventListener("focus", () => {
+    document.body.classList.remove("blur")
+  })
+})
---
+++ src/terminal.ts
@@ -0,0 +1,262 @@
+type TerminalInputHandler = (terminal: Terminal, commandString: string, name: string, args: string[]) => void
+
+const contactPrompt = (terminal: Terminal) => terminal.prompt("Type your name:", null, (name: string) => {
+  terminal.prompt("Enter your email:", consts.emailRegex, (email) => {
+    terminal.prompt("Message:", null, (message) => {
+      terminal.prompt("Confirm send message? [y/n]", consts.confirmationRegex, (confirmation) => {
+        if (confirmation !== "y") {
+          terminal.addText("No worries! Let's start over.")
+          contactPrompt(terminal)
+
+          return
+        }
+
+        fetch("https://api.staticforms.xyz/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            // Don't worry, this isn't secret :).
+            accessKey: "bee85cdb-bd13-4bbe-896b-d159c35577d0",
+            subject: "Contact form submission from website",
+            replyTo: "@",
+            name,
+            email,
+            message
+          }),
+        })
+          .then((response) =>
+            terminal.addText(response.ok ? "Your message was successfully sent. You will receive a response via e-mail soon. Thank you!" : `Message was sent but the service did not provide a success response: ${response.statusText} (${response.status})`)
+          )
+          .catch((error) => terminal.addText(`There was an error while sending your message: ${error}`))
+          .finally(Terminal.defaultPrompt)
+      })
+    })
+  })
+})
+
+class Terminal {
+  static defaultPrompt(terminal: Terminal) {
+    terminal.prompt("->", null, (response) => {
+      const parts = response.split(" ")
+      const name = parts[0]
+      const args = parts.slice(1)
+
+      Terminal.defaultCommandHandler(terminal, response, name, args)
+    })
+  }
+
+  static defaultCommandHandler(terminal: Terminal, _commandString: string, name: string, _args: string[]) {
+    switch (name) {
+      case "help": {
+        for (const [name, helpMessage] of consts.terminalCommands) {
+          terminal.addText(name, true, false)
+          terminal.addText(`: ${helpMessage}`)
+        }
+
+        break
+      }
+      case "clear": {
+        // TODO: Need to always have a meta child to keep the caret.
+
+        terminal.clear()
+
+        break
+      }
+      case "send-msg": {
+        contactPrompt(terminal)
+
+        return
+      }
+      case "view-resume": {
+        window.location.href = "./resume.pdf"
+
+        return
+      }
+      case "view-source": {
+        window.open("https://github.com/yurixander/yurixander.dev", "_blank")
+
+        return
+      }
+      default: terminal.addText(`No command matching name '${name}' is registered. Type 'help' for a list of available commands.`)
+    }
+
+    Terminal.defaultPrompt(terminal)
+  }
+
+  $terminal: HTMLElement
+  inputHandler: TerminalInputHandler
+  inputBuffer: string
+  isReadonly = true
+
+  constructor(state: State, $terminal: HTMLElement) {
+    this.$terminal = $terminal
+
+    this.$terminal.addEventListener("keydown", (e) => {
+      // Ignore input keystrokes when the terminal is marked as read-only.
+      if (this.isReadonly) {
+        state.$clickAudio.play()
+
+        return
+      }
+
+      // REVIEW: Is this still required? It's blocking alt-tabs and so on when writing to a terminal.
+      // Prevents from scrolling when space is pressed.
+      // e.preventDefault()
+
+      // Erase.
+      if (e.key === "Backspace") {
+        // Using query selector fixes the fact that the last child is
+        // the text content, and not an HTML node itself.
+        let $lastChild = document.querySelector(".terminal > .content > *:last-child")
+
+        if ($lastChild.hasAttribute("data-non-deletable")) {
+          state.$clickAudio.play()
+
+          return
+        }
+
+        this.inputBuffer = this.inputBuffer.substring(0, this.inputBuffer.length - 1)
+
+        $lastChild.remove()
+
+        return
+      }
+      // Submit command.
+      else if (e.key === "Enter") {
+        const commandString = this.inputBuffer.trim()
+
+        // Ignore empty input.
+        if (commandString === "")
+          return
+
+        this.inputBuffer = ""
+
+        const parts = commandString.split(" ")
+        const name = parts[0]
+        const args = parts.slice(1)
+
+        this.appendNewline()
+
+        if (this.inputHandler !== null) {
+          const handler = this.inputHandler
+
+          this.inputHandler = Terminal.defaultCommandHandler
+          handler(this, commandString, name, args)
+        }
+
+        return
+      }
+      // Ignore other keys.
+      else if (e.key.length > 1)
+        return
+
+      // At this point, we can register the key to the input buffer.
+      this.inputBuffer += e.key
+
+      let $character = document.createElement("span")
+
+      // Edge case for space.
+      if (e.key === " ") {
+        this.appendSpace(true)
+
+        return
+      }
+      else
+        $character.textContent = e.key
+
+      this.$terminal.appendChild($character)
+      this.scroll()
+    })
+  }
+
+  clear() {
+    while (this.$terminal.lastChild)
+      this.$terminal.removeChild(this.$terminal.lastChild)
+  }
+
+  scroll() {
+    this.$terminal.scrollTop = this.$terminal.scrollHeight
+  }
+
+  addText(text: string, isBold: boolean = false, includeNewline: boolean = true): void {
+    let $text = document.createElement(isBold ? "strong" : "span")
+
+    $text.toggleAttribute("data-non-deletable")
+    $text.textContent = text
+    this.$terminal.appendChild($text)
+    this.scroll()
+
+    if (includeNewline)
+      this.appendNewline()
+  }
+
+  appendSpecialCharacter(tagName: string, classNameOrNull: string | null = null, isDeletable: boolean = false): void {
+    let $specialCharacter = document.createElement(tagName)
+
+    if (classNameOrNull !== null)
+      $specialCharacter.classList.add(classNameOrNull)
+
+    if (!isDeletable)
+      $specialCharacter.setAttribute("data-non-deletable", "")
+
+    this.$terminal.appendChild($specialCharacter)
+    this.scroll()
+  }
+
+  appendNewline(): void {
+    this.appendSpecialCharacter("br")
+  }
+
+  appendSpace(isDeletable: boolean = false): void {
+    this.appendSpecialCharacter("span", "space", isDeletable)
+  }
+
+  prompt(text: string, formatOrNull: RegExp | null, callback: (response: string) => void): void {
+    const addPromptMessage = () => {
+      this.addText(text, true, false)
+      this.appendSpace()
+    }
+
+    addPromptMessage()
+
+    const handler: TerminalInputHandler = (_, commandString, _name, _args) => {
+      if (formatOrNull !== null && !formatOrNull.test(commandString)) {
+        this.addText("Sorry, it appears that your input is not properly formatted. Please verify that there aren't any extra characters, and try again.")
+
+        addPromptMessage()
+        this.inputHandler = handler
+      }
+      else
+        callback(commandString)
+    }
+
+    this.inputHandler = handler
+  }
+}
+
+class Timeline {
+  terminal: Terminal
+  actions: Promise<void>[]
+
+  constructor(terminal: Terminal) {
+    this.terminal = terminal
+    this.actions = []
+  }
+
+  type(commandString: string): Timeline {
+    this.actions.push(new Promise<void>((resolve: () => void) => {
+      commandString.split("").forEach((char: string) => {
+        this.terminal.addText(char, false, false)
+        setTimeout(() => resolve(), consts.terminalKeystrokeInterval)
+      })
+    }))
+
+    return this
+  }
+
+  run() {
+    Promise.all(this.actions)
+  }
+}
